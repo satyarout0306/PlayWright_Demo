00d34f4abe1a5fb24dad0f79a32a599b7dcd8126
SatyaRout <satyarout@SatyaRouts-MacBook-Pro.local>
2024-07-06 17:59:45 +0530

    headless as false

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    headless: true,
+    headless: false,
     viewport: { width: 1280, height: 720 },
     video: 'retain-on-failure',
     screenshot: 'only-on-failure',
