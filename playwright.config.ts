69e286666ba44dbefa9a715d5eac1a33d94028ec
SatyaRout <satyarout@SatyaRouts-MacBook-Pro.local>
2024-07-07 11:04:08 +0530

    Added Allur report setup

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,8 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-    ['html', { outputFolder: 'playwright-report' }]
+    ['html', { outputFolder: 'playwright-report' }],
+    ['allure-playwright'],
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
